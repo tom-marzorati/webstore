7f7a9c1166037e2035b2282303831cb857d7de45
tom-marzorati <46445512+tom-marzorati@users.noreply.github.com>
2019-01-09 17:07:47 +0100

    Update SessionUtil.java

--- SessionUtil.java
+++ SessionUtil.java
@@ -1,49 +1,48 @@
-package com.accenture.utility;
-
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-
-
-public class SessionUtil {
+//singleton (?)
+	private static StandardServiceRegistry registry;
+	private static SessionFactory sessionFactory ;
 	
-
+	public static SessionFactory getSessionFactory() {
+		/*
+		 * HOW TO:
+		 * 
+		 * 	1) Initialize Registry
+		 *  2) Create Metadata Source
+		 *  3) Create Metadata
+		 *  4) Create SessionFactory type object
+		 *  
+		 *  
+		 */
+		if (sessionFactory==null){
+			try {
+				// 1)
+				registry = new StandardServiceRegistryBuilder().configure().build();
+			
+				// 2)
+				MetadataSources source = new MetadataSources(registry);
+				
+				// 3)
+				Metadata metadata = source.getMetadataBuilder().build();
+				
+				// 4)
+				SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();			
+				return sessionFactory;
+			} catch (Exception e) {
+				e.printStackTrace();
+		        if (registry != null) {
+		          StandardServiceRegistryBuilder.destroy(registry);
+		        }
+			}
+		}			
+		return sessionFactory;
+	}//End of getSessionFactory
 	
-	  private static StandardServiceRegistry registry;
-	  private static SessionFactory sessionFactory;
-
-	  public static SessionFactory getSessionFactory() {
-	    if (sessionFactory == null) {
-	      try {
-	        // Create registry
-	        registry = new StandardServiceRegistryBuilder()
-	            .configure()
-	            .build();
-
-	        // Create MetadataSources
-	        MetadataSources sources = new MetadataSources(registry);
-
-	        // Create Metadata
-	        Metadata metadata = sources.getMetadataBuilder().build();
-
-	        // Create SessionFactory
-	        sessionFactory = metadata.getSessionFactoryBuilder().build();
-
-	      } catch (Exception e) {
-	        e.printStackTrace();
-	        if (registry != null) {
-	          StandardServiceRegistryBuilder.destroy(registry);
-	        }
-	      }
-	    }
-	    return sessionFactory;
-	  }
-
-	  public static void shutdown() {
-	    if (registry != null) {
+	//what's the use of this tho?
+	public static void shutdown() {
+	    if (registry != null ) {
 	      StandardServiceRegistryBuilder.destroy(registry);
-	      }
-      }
+	      sessionFactory.close();
+	    }
+	}//end of method i don't really get
+	
 }
